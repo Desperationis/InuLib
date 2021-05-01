ce28d070bf56a42b6a5ad78c9a105e589dc2d3ea
Diego Contreras <smarttdiego@gmail.com>
2021-04-30 22:01:38 -0700

    Add ASCII decoding t readUart.py
    While you could read output of readUart.py normally,
    it was still encoded in byte form. With this, all
    output will be decoded into ASCII characters.

--- RaspberryPi/readUart.py
+++ RaspberryPi/readUart.py
@@ -1,6 +1,6 @@
 import pygame
 import serial
-import time
+import sys
 
 # Start main loop to send serial data to the Cortex
 serialInterface = serial.Serial('/dev/ttyS0',9600,timeout = 0,stopbits=1,bytesize=8)
@@ -12,7 +12,7 @@ while True:
     if serialInterface.isOpen:
         #serialInterface.write([254]) # Header Byte
         #serialInterface.write([69])
-        print(serialInterface.read())
+        print(serialInterface.read(int(sys.argv[1])).decode('ascii'))
     else:
         print("Error: UART Serial interface is not open.")
 
