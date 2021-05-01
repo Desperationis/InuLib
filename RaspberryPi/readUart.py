5ebdb04561ec1909f5927ed6ea328ec52ab4d98e
Diego Contreras <smarttdiego@gmail.com>
2021-04-30 22:50:01 -0700

    Have readUart.py read until \n
    To have lines of variable length, assume all
    output from the cortex has a newline denoting
    the end of a line.

--- RaspberryPi/readUart.py
+++ RaspberryPi/readUart.py
@@ -3,7 +3,7 @@ import serial
 import sys
 
 # Start main loop to send serial data to the Cortex
-serialInterface = serial.Serial('/dev/ttyS0',9600,timeout = 0,stopbits=1,bytesize=8)
+serialInterface = serial.Serial('/dev/ttyS0',9600,timeout = 1,stopbits=1,bytesize=8)
 
 pygame.init()
 
@@ -12,7 +12,7 @@ while True:
     if serialInterface.isOpen:
         #serialInterface.write([254]) # Header Byte
         #serialInterface.write([69])
-        print(serialInterface.read(int(sys.argv[1])).decode('ascii'))
+        print(serialInterface.readline().decode('ascii'),end='')
     else:
         print("Error: UART Serial interface is not open.")
 
