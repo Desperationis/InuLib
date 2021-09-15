5aec2209f01d02e1f99c50c7145b70752ee73298
Desperationis <smarttdiego@gmail.com>
2021-09-15 09:29:45 -0700

    Fully implement ControllerCallback.cpp

--- include/ControllerCallback.h
+++ include/ControllerCallback.h
@@ -10,12 +10,21 @@
 #include "main.h"
 #include "pros/misc.h"
 #include "pros/misc.hpp"
+#include <map>
+#include <array>
 
 /**
  * System for adding asynchonous and synchronous callback function to
  * controller events.
 */ 
 class ControllerCallback {
+private:
+	struct CallbackInfo {
+		pros::task_fn_t func;
+		bool async = false;
+		bool buttonPressed = false;
+	};
+
 public:
 	ControllerCallback(pros::controller_id_e_t id);
 
@@ -23,9 +32,26 @@ public:
 
 	void SyncCallback(pros::controller_digital_e_t button, pros::task_fn_t func);
 
+	void PollController();
+
 private:
 	pros::Controller controller;
-	pros::Task* asyncTask = nullptr;
+	std::map<pros::controller_digital_e_t, CallbackInfo> callbackButtonMap;
+
+	const std::array<pros::controller_digital_e_t, 12> DIGITAL = {
+		pros::E_CONTROLLER_DIGITAL_L1,
+		pros::E_CONTROLLER_DIGITAL_L2,
+		pros::E_CONTROLLER_DIGITAL_R1,
+		pros::E_CONTROLLER_DIGITAL_R2,
+		pros::E_CONTROLLER_DIGITAL_UP,
+		pros::E_CONTROLLER_DIGITAL_DOWN,
+		pros::E_CONTROLLER_DIGITAL_LEFT,
+		pros::E_CONTROLLER_DIGITAL_RIGHT,
+		pros::E_CONTROLLER_DIGITAL_X,
+		pros::E_CONTROLLER_DIGITAL_B,
+		pros::E_CONTROLLER_DIGITAL_Y,
+		pros::E_CONTROLLER_DIGITAL_A,
+	};
 };
 
 
---
+++ src/ControllerCallback.cpp
@@ -0,0 +1,54 @@
+#include "ControllerCallback.h"
+#include "pros/misc.h"
+
+using namespace pros;
+
+ControllerCallback::ControllerCallback(controller_id_e_t id) : controller(id) {
+}
+
+
+void ControllerCallback::AsyncCallback(controller_digital_e_t button, task_fn_t func) {
+	if(callbackButtonMap.find(button) != callbackButtonMap.end()) {
+		CallbackInfo callbackInfo;
+		callbackInfo.async = true;
+		callbackInfo.func = func;
+
+		callbackButtonMap[button] = callbackInfo;
+	}
+}
+
+void ControllerCallback::SyncCallback(controller_digital_e_t button, task_fn_t func) {
+	if(callbackButtonMap.find(button) != callbackButtonMap.end()) {
+		CallbackInfo callbackInfo;
+		callbackInfo.async = false;
+		callbackInfo.func = func;
+
+		callbackButtonMap[button] = callbackInfo;
+	}
+}
+
+
+void ControllerCallback::PollController() {
+	for(int i = 0; i < DIGITAL.size(); i++) {
+		controller_digital_e_t button = DIGITAL[i];
+
+		if(callbackButtonMap.find(button) != callbackButtonMap.end()) {
+			CallbackInfo cInfo = callbackButtonMap[button];
+
+			if(controller.get_digital(button) && cInfo.buttonPressed == false) {
+				// Sync Case
+				if(cInfo.async == false) {
+					cInfo.func(nullptr);
+				}
+
+				// Async Case
+				else {
+					Task asyncTask(cInfo.func, nullptr, "");
+				}
+			}
+
+			cInfo.buttonPressed = controller.get_digital(button);
+		}
+	}
+
+}
