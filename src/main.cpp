3c11b566c799cbe607115885cb9187b0d8c6b438
Diego Contreras <smarttdiego@gmail.com>
2021-11-04 09:48:53 -0700

    Removed old competition code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -18,34 +18,6 @@ void initialize() {
 	pros::lcd::initialize();
 }
 
-/**
- * Automatically retract claw until it hits a limit switch. Once it does, open
- * up the claw.
-*/ 
-void clawTurn(void* param) {
-	SlewMotor arm(11);
-	pros::ADIMotor claw(1);
-	pros::ADIDigitalIn sensor(2);
-	pros::Controller controller(pros::E_CONTROLLER_MASTER);
-	while(true) {
-		arm.Set(70);
-		ControllerStream::Print(pros::E_CONTROLLER_MASTER, "Lifting arm; Wait");
-
-		if(sensor.get_value() != 0) {
-			arm.Set(0);
-			pros::delay(400);
-			claw.set_value(80);
-			pros::delay(900);
-			ControllerStream::Print(pros::E_CONTROLLER_MASTER, "");
-			claw.set_value(0);
-			break;
-		}
-		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) 
-			break;
-
-		pros::delay(5);
-	}
-}
 
 /**
  * If no competition control is connected, this function will run immediately
@@ -59,7 +31,6 @@ void opcontrol() {
 	ControllerStream::Start();
 	SlewSystem::Start();
 	ControllerCallback callback(pros::E_CONTROLLER_MASTER);
-	callback.SyncCallback(pros::E_CONTROLLER_DIGITAL_A, clawTurn);
 	pros::ADIMotor claw(1);
 	pros::Motor armOG(11); // Don't move arm with this; Only for absolute position
 
@@ -84,8 +55,6 @@ void opcontrol() {
 		SlewMotor topright(10);
 		SlewMotor bottomright(12);
 		SlewMotor bottomleft(19);
-		SlewMotor basket(20);
-		SlewMotor camera(14);
 
 		// X-Drive Controller Code
 		pros::Controller controller(pros::E_CONTROLLER_MASTER);
@@ -99,17 +68,7 @@ void opcontrol() {
 		bottomright.Set(-x - y + turn);
 
 		// Arm, claw, and basket code.
-		basket.Set(0);
 		claw.set_value(0);
-		camera.Set(0);
-
-		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
-			basket.Set(40);
-		}
-	
-		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
-			basket.Set(-40);
-		}
 
 		// Don't use arm.GetTarget() as it's set to 0 every iteration
 		// of this loop; Dangerous
@@ -132,18 +91,6 @@ void opcontrol() {
 		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
 			claw.set_value(-100);
 		}
-
-		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_X)) {
-			camera.Set(-60);
-		}
-		
-		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
-			camera.Set(60);
-		}
-	
-
-		pros::lcd::print(2, "%f", (float)armTarget);
-		pros::lcd::print(3, "%f", (float)arm.GetTarget());
 
 		arm._UpdatePID();
 
