215668e074c5765d2ad4f25a92afe6a18356c56d
Diego Contreras <smarttdiego@gmail.com>
2022-01-18 09:38:16 -0800

    Started adding code for new competition xdrive

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,12 @@
 #include "main.h"
 #include "inu/InuException.hpp"
 #include "inu/Color.hpp"
-#include "inu/motor/background/PIDVisionMotor.h"
-#include "inu/motor/background/SlewMotor.h"
+#include "inu/motor/background/PIDMotor.h"
 #include "inu/motor/PIDProfile.hpp"
-#include "inu/auto/chassis/AutoXChassis.h"
-#include "inu/auto/chassis/AutoXChassisBuilder.h"
-#include "inu/auto/XLineFollower.h"
-#include "inu/auto/XLineFollowerBuilder.h"
-#include "inu/wrapper/LineSensor.h"
 #include "inu/wrapper/Motor.h"
-#include "inu/wrapper/VisionSensor.h"
-#include "inu/wrapper/InertialSensor.h"
-#include "inu/auto/ArmAssembly.h"
-#include "inu/auto/ArmAssemblyBuilder.h"
-#include "inu/auto/ColorFollower.h"
-#include "inu/auto/ColorFollowerBuilder.h"
-#include "inu/background/CameraRainbowFlex.h"
 #include "pros/colors.h"
 #include "pros/llemu.hpp"
+#include "pros/misc.h"
 #include "pros/motors.h"
 #include "pros/rtos.hpp"
 #include "pros/vision.h"
@@ -54,165 +42,40 @@ void initialize() {
 	pros::delay(3000);
 }
 
-void CalibrateThreshold(std::shared_ptr<XLineFollower> follower) {
-	while(true) {
-		bool detected = follower->LineDetected();
-		pros::lcd::print(0, "Line detected: %d", (int)detected);
-
-		int value = follower->RecommendThreshold();
-		pros::lcd::print(6, "Recommended midpoint: %d", (int) value);
-
-		pros::delay(10);
-	}
-}
-
-/**
- * Given that the claw was set to a position, check if it reached that position,
- * and if so, grab the object, retract the arm, and let go.
-*/ 
-void Collect(std::shared_ptr<ArmAssembly> armAssembly) {
-	while(!armAssembly->AtTarget(10)) {
-		pros::delay(20);
-	}
-	armAssembly->Grab();
-	armAssembly->Retract();
-	armAssembly->Release();
-}
-
 void opcontrol() {
-	try {
-		CameraRainbowFlex flex1(9);
-		CameraRainbowFlex flex2(12);
-	
-		AutoChassisBuilder::AutoChassisOptions chassisOptions;
-		chassisOptions.maxVelocity = 90;
-		chassisOptions.timeoutLimit = 10;
-		chassisOptions.timeoutAlignLimit = 0.5;
-		chassisOptions.stalls = true;
-		//chassisOptions.steadyStateEncoderError = 10;
-		chassisOptions.steadyStateEncoderError = 8;
+		inu::Motor topleft(11);
+		inu::Motor topright(20);
+		inu::Motor bottomleft(3);
+		inu::Motor bottomright(4);
 
-		AutoChassisBuilder::AutoChassisGyroOptions gyroOptions;
-		gyroOptions.gyroPID.p = 1;
-		gyroOptions.gyroPID.i = 0.10;
-		gyroOptions.gyroPID.d = 0;
-		gyroOptions.gyroPID.integralWindupLimit = 30;
-		gyroOptions.gyroPID.integralLevelingError = 0;
-		gyroOptions.steadyStateAngleError = 2;
+		inu::PIDProfile armProfile;
+		armProfile.p = 0.9;
+		armProfile.i = 0.0;
+		armProfile.d = 0.0;
+		inu::PIDMotor arm(10, armProfile);
 
-		ArmAssemblyBuilder armBuilder;
-		armBuilder.SetArmMotor(18, PIDProfile(0.7f));
-		armBuilder.SetClawMotor(13);
-		armBuilder.SetButton('E');
-		armBuilder.SetArmMaximumVelocity(70);
-		std::shared_ptr<ArmAssembly> armAssembly = armBuilder.Build();
+		while(true) {
+			pros::Controller controller(pros::E_CONTROLLER_MASTER);
+			int x = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+			int y = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+			int turn = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
-		AutoXChassisBuilder builder;
-		builder.SetMotors(11,20,3,4);
-		builder.SetGyro(10, gyroOptions);
-		builder.SetChassisOptions(chassisOptions);
+			topleft.Move(std::clamp<int>(x + y + turn, -70, 70));
+			topright.Move(std::clamp<int>(x - y + turn, -70, 70));
+			bottomleft.Move(std::clamp<int>(-x + y + turn, -70, 70));
+			bottomright.Move(std::clamp<int>(-x - y + turn, -70, 70));
 
-		std::shared_ptr<AutoXChassis> chassis = builder.Build();
+			arm.Set(arm.GetPosition());
 
-		XLineFollowerBuilder followerBuilder;
-		followerBuilder.SetSensors( { 'B', 'F', 'G', 'H', 'A'} );
-		followerBuilder.SetSensorError( { -30, -190, 0, 60, -450 } );
-		followerBuilder.SetChassis(std::weak_ptr(chassis));
-		followerBuilder.ActivateOnDark(false);
-		followerBuilder.SetLightThreshold(600);
-		
-		std::shared_ptr<XLineFollower> follower = followerBuilder.Build();
+			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+				arm.Set(arm.GetPosition() - 50);
+			}
+			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+				arm.Set(arm.GetPosition() + 50);
+			}
 
-		// Go to headquarters
-		chassis->TurnA(45);
-		//follower->FollowLine(45);
-		follower->FollowLine(45);
-		chassis->TurnA(-360);
-		armAssembly->MoveArm(2550);
-		chassis->TurnA(-130);
-
-		// Collect the first block on the bottomleft of headquarters 
-		chassis->Forward(100);
-		Collect(armAssembly);
-
-		// Follow line till you get to the double blocks
-		armAssembly->MoveArm(2250);
-		if(!follower->LineDetected())
-			throw InuException("AHHH");
-		follower->FollowLine(1600, 45);
-
-		// Collect top block
-		Collect(armAssembly);
-
-		// Collect Second block
-		armAssembly->MoveArm(2650);
-		chassis->Forward(100);
-		Collect(armAssembly);
-
-		// Follow line to get to the brick in front of the cats
-		if(!follower->LineDetected())
-			throw InuException("NOOOO");
-		armAssembly->MoveArm(2550);
-		follower->FollowLine(1750, 40);
-
-		// Collect the brick
-		Collect(armAssembly);
-
-		// Go all the way to the cats.
-		if(!follower->LineDetected())
-			throw InuException("UUUUUUUU");
-		armAssembly->MoveArm(1650);
-		follower->FollowLine(45);
-
-		// Angle up arm, then turn around to get the cats.
-		while(!armAssembly->AtTarget(10)) {
-			pros::delay(20);
+			pros::delay(10);
 		}
-		chassis->TurnA(-360);
-
-		// Go to cup with claw open
-		chassis->TurnA(135);
-		armAssembly->MoveArm(850);
-		follower->FollowLine(2750, 45);
-
-		// Lift up cup
-		armAssembly->LightlyGrab();
-		armAssembly->MoveArm(-900);
-		while(!armAssembly->AtTarget(10)) {
-			pros::delay(20);
-		}
-		follower->FollowLine(370, 30);
-		chassis->TurnA(-18);
-		armAssembly->MoveArm(700);
-		while(!armAssembly->AtTarget(10)) {
-			pros::delay(20);
-		}
-		armAssembly->Release();
-
-		// Go back in line, and follow it to the topleft corner.
-		armAssembly->Retract();
-		chassis->TurnA(18);
-		follower->FollowLine(40);
-
-		// Face the next row of bricks and grab the first one.
-		armAssembly->MoveArm(2550);
-		chassis->TurnA(135);
-		Collect(armAssembly);
-
-		// Go all the back to the brick back at day job
-		follower->FollowLine(3450, 50);
-		armAssembly->MoveArm(2550);
-		Collect(armAssembly);
-
-		// Go back to headquarters
-		follower->FollowLine(45);
-		chassis->TurnA(-135);
-		follower->FollowLine(45);
-	}
-	catch(InuException e) {
-		std::cout << Color::FG_RED << e.what() << Color::FG_DEFAULT << std::endl;
-		throw std::runtime_error("InuException thrown");
-	}
 
 
 	// In case we exit; Delay indefinitely so that the motors don't get 
